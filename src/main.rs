52e8b72b595cc550a80114c7b3204d3e08012706
Zachary D. Skelton <zbert@hotmail.com>
2022-02-05 14:58:41 -0600

    build(clock): Working Version

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 use chrono::prelude::*;
+use ansi_term::Colour;
 
-fn main() {
+/* PROCESS FUNCTIONS */
+fn display_time() -> Result<(), String> {
     let local: DateTime<Local> = Local::now();
+    let disp = local.to_rfc2822();
 
-    println!("Localtime: {}", local);
+    println!("{disp}\n");
+    // debug_show(format!("{}\n", disp));
+
+    Ok(())
+}
+
+/* MAIN */
+fn main() {
+    // Display Time
+    if !display_time().is_ok() {
+        println!("{}", Colour::Red.paint(format!("Error: Date did not compute.\n")));
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert!(true);
+    fn program_works() {
+        let _res = match display_time() {
+            Ok(_) => true,
+            Err(_) => false
+        };
+        assert!(_res);
     }
 }
